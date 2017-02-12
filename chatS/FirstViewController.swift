7f5e2e64bb9eb8f08acb8800eea7cf1ba8e8b684
Dinesh Namburi <n.dinesh36@gmail.com>
2017-02-11 23:18:25 -0500

    First Screen ready

---
+++ chatS/FirstViewController.swift
@@ -0,0 +1,48 @@
+//
+//  FirstViewController.swift
+//  chatS
+//
+//  Created by dinesh on 11/02/17.
+//  Copyright © 2017 dinesh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var fbButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var joinButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
+        //fbLabel.layer.cornerRadius = 50
+        fbButton.layer.cornerRadius = 19
+        loginButton.layer.cornerRadius = 19
+        joinButton.layer.cornerRadius = 19
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
